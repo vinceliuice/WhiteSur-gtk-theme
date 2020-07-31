002c9a4c8502fc9daa375dd75e177e0ae97d5c3a
vinceliuice <vinceliuice@hotmail.com>
2020-07-31 15:45:58 +0800

    update

--- src/assets/gtk-3.0/common-assets/render-assets.sh
+++ src/assets/gtk-3.0/common-assets/render-assets.sh
@@ -18,8 +18,8 @@ else
     echo Rendering $ASSETS_DIR/$i.png
     $INKSCAPE --export-id=$i \
               --export-id-only \
-              --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
-    && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png 
+              --export-filename=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null
+    $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png 
 fi
 if [ -f $ASSETS_DIR/$i@2.png ]; then
     echo $ASSETS_DIR/$i@2.png exists.
@@ -29,8 +29,8 @@ else
     $INKSCAPE --export-id=$i \
               --export-dpi=180 \
               --export-id-only \
-              --export-png=$ASSETS_DIR/$i@2.png $SRC_FILE >/dev/null \
-    && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i@2.png 
+              --export-filename=$ASSETS_DIR/$i@2.png $SRC_FILE >/dev/null
+    $OPTIPNG -o7 --quiet $ASSETS_DIR/$i@2.png 
 fi
 done
 exit 0
