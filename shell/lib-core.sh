6d5201848605cd4ad4d90cac2e01636a52d38f64
vinceliuice <vinceliuice@hotmail.com>
2024-03-29 01:49:55 +0800

    Fixed gnome-shell 46 issues

--- shell/lib-core.sh
+++ shell/lib-core.sh
@@ -26,7 +26,9 @@ MY_HOME=$(getent passwd "${MY_USERNAME}" | cut -d: -f6)
 
 if command -v gnome-shell &> /dev/null; then
   SHELL_VERSION="$(gnome-shell --version | cut -d ' ' -f 3 | cut -d . -f -1)"
-  if [[ "${SHELL_VERSION:-}" -ge "44" ]]; then
+  if [[ "${SHELL_VERSION:-}" -ge "46" ]]; then
+    GNOME_VERSION="46-0"
+  elif [[ "${SHELL_VERSION:-}" -ge "44" ]]; then
     GNOME_VERSION="44-0"
   elif [[ "${SHELL_VERSION:-}" -ge "42" ]]; then
     GNOME_VERSION="42-0"
