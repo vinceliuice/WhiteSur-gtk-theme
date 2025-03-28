8ff1fe69ab822c4c110e51482d9ac046ef8d88b6
vinceliuice <vinceliuice@hotmail.com>
2025-03-28 14:56:20 +0800

    Fixed #1245, fixed #1237

--- libs/lib-core.sh
+++ libs/lib-core.sh
@@ -26,7 +26,9 @@ MY_HOME=$(getent passwd "${MY_USERNAME}" | cut -d: -f6)
 
 if command -v gnome-shell &> /dev/null; then
   SHELL_VERSION="$(gnome-shell --version | cut -d ' ' -f 3 | cut -d . -f -1)"
-  if [[ "${SHELL_VERSION:-}" -ge "47" ]]; then
+  if [[ "${SHELL_VERSION:-}" -ge "48" ]]; then
+    GNOME_VERSION="48-0"
+  elif [[ "${SHELL_VERSION:-}" -ge "47" ]]; then
     GNOME_VERSION="47-0"
   elif [[ "${SHELL_VERSION:-}" -ge "46" ]]; then
     GNOME_VERSION="46-0"
@@ -40,7 +42,7 @@ if command -v gnome-shell &> /dev/null; then
     GNOME_VERSION="3-28"
   fi
 else
-  GNOME_VERSION="47-0"
+  GNOME_VERSION="48-0"
 fi
 
 #----------Program options-------------#
